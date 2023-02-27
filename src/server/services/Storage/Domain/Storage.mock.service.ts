adf5361f8aa19afebb9cb276beb0091fddef1c0f
alejandrobailo <alejandrobailo94@gmail.com>
2023-02-27 11:33:00 +0100

    feat: cognito pool

--- src/server/services/Storage/Domain/Storage.mock.service.ts
+++ src/server/services/Storage/Domain/Storage.mock.service.ts
@@ -3,16 +3,16 @@ import { marshall } from "@aws-sdk/util-dynamodb";
 import { StorageService } from "./Storage.service";
 import { userMock } from "../../Users/Domain/Users.service.mock";
 
-const marshalledUserMock = marshall(userMock);
+const marshalledUserMock = marshall(JSON.stringify(userMock), {
+  removeUndefinedValues: true
+});
 
 export const dynamoClientMock = {
   putItem: jest.fn().mockResolvedValue({ Item: marshalledUserMock }),
   scan: jest.fn().mockResolvedValue({ Items: [marshalledUserMock] }),
   getItem: jest.fn().mockResolvedValue({ Item: marshalledUserMock }),
-  deleteItem: jest
-    .fn()
-    .mockResolvedValue({ Item: marshall({ marshalledUserMock }) }),
-  updateItem: jest.fn().mockResolvedValue({ Item: marshalledUserMock }),
+  deleteItem: jest.fn().mockResolvedValue({ Item: { marshalledUserMock } }),
+  updateItem: jest.fn().mockResolvedValue({ Item: marshalledUserMock })
 } as unknown as DynamoDB;
 
 export const storageServiceMock = new StorageService(dynamoClientMock);
